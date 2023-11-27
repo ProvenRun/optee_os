ee230e4ab8569371b75e324e7a533c2112c4c4a7
Jeremie Corbier <jeremie.corbier@provenrun.com>
2023-11-27 10:38:05 +0100

    drivers: versal: split ecc driver into pki and mbox variants
    Signed-off-by: Jeremie Corbier <jeremie.corbier@provenrun.com>

---
+++ core/drivers/crypto/versal/include/ecc.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) ProvenRun SAS, 2023
+ */
+
+#ifndef ECC_H
+#define ECC_H
+
+TEE_Result versal_ecc_get_key_size(uint32_t curve, size_t *bytes, size_t *bits);
+TEE_Result versal_ecc_prepare_msg(uint32_t algo, const uint8_t *msg,
+				  size_t msg_len, size_t *len, uint8_t *buf);
+
+TEE_Result versal_ecc_hw_init(void);
+TEE_Result versal_ecc_kat(void);
+
+TEE_Result versal_ecc_verify(uint32_t algo, struct ecc_public_key *key,
+			 const uint8_t *msg, size_t msg_len,
+			 const uint8_t *sig, size_t sig_len);
+TEE_Result versal_ecc_sign(uint32_t algo, struct ecc_keypair *key,
+		       const uint8_t *msg, size_t msg_len,
+		       uint8_t *sig, size_t *sig_len);
+
+void memcpy_swp(uint8_t *to, const uint8_t *from, size_t len);
+void crypto_bignum_bn2bin_eswap(uint32_t curve, struct bignum *from, uint8_t *to);
+
+#endif
--- core/drivers/crypto/versal/sub.mk
+++ core/drivers/crypto/versal/sub.mk
@@ -3,4 +3,9 @@ incdirs-y += include
 srcs-y += ipi.c
 srcs-y += authenc.c
 srcs-y += ecc.c
+ifeq ($(PLATFORM_FLAVOR),adaptative)
+srcs-y += ecc_pki_engine.c
+else
+srcs-y += ecc_mbox.c
+endif
 srcs-y += rsa.c
